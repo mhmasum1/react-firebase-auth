8c15b63f3226f20c58bce7d228f9fd3a7b8d6f78
mhmasum1 <mhmasum231@gmail.com>
2025-10-18 12:18:49 +0600

    Use Context to create a user on the register page

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,29 +1,46 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth/cordova';
+// import { createUserWithEmailAndPassword } from 'firebase/auth/cordova';
 import React, { use } from 'react';
 import { Link } from 'react-router';
-import { auth } from '../../firebase/firebase.init';
+// import { auth } from '../../firebase/firebase.init';
 import { AuthContext } from '../../contexts/AuthContext';
 
 const Register = () => {
-    const userInfo = use(AuthContext)
-    console.log(userInfo);
+    // const userInfo = use(AuthContext)
+    // console.log(userInfo);
 
-    const handleRegister = (e) => {
+    const { createUser } = use(AuthContext)
+
+    const handleRegister = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
-
-        createUserWithEmailAndPassword(auth, email, password)
+        createUser(email, password)
             .then(result => {
-                console.log(result);
+                console.log(result.user);
             })
             .catch(error => {
                 console.log(error);
-            });
-
+            })
 
     }
+
+    // const handleRegister = (e) => {
+    //     e.preventDefault();
+    //     const email = e.target.email.value;
+    //     const password = e.target.password.value;
+    //     console.log(email, password);
+
+    //     createUserWithEmailAndPassword(auth, email, password)
+    //         .then(result => {
+    //             console.log(result);
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+
+
+    // }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword } from 'firebase/auth/web-extension';
+import { auth } from '../firebase/firebase.init';
 
 const AuthProvider = ({ children }) => {
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
     const userInfo = {
-        email: 'masum231@gmail.com'
+        createUser,
     }
     return (
         <AuthContext value={userInfo}>
